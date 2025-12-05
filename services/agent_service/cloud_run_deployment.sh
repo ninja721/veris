7512db7aa3af1307d8ae046449b4bc8c5faeb658
Deepak <luexclothings@gmail.com>
2025-12-05 23:36:11 +0000

    chore(deploy): Update deploy scripts

--- services/agent_service/cloud_run_deployment.sh
+++ services/agent_service/cloud_run_deployment.sh
@@ -1,12 +1,23 @@
 adk deploy cloud_run \
   --project=veris-478615 \
   --region=us-central1 \
-  --service_name=veris-ai\
+  --service_name=veris-ai \
   --app_name=veris_agent \
   --with_ui \
-  --allow_origins=* \
+  --allow_origins=https://veris.luex.shop \
   .
 
-gcloud builds submit --tag gcr.io/pungde-477205/pungda-predict-service 
+gcloud run deploy veris-web \
+  --source . \
+  --region us-central1 \
+  --platform managed \
+  --allow-unauthenticated
 
-gcloud run deploy pungda-predict-service   --image gcr.io/pungde-477205/pungda-predict-service   --platform managed   --region us-central1   --allow-unauthenticated
+gcloud run deploy veris-crawler \
+  --source . \
+  --region us-central1 \
+  --platform managed \
+  --allow-unauthenticated
+
+gsutil cors set cors.json gs://veris-media
+
